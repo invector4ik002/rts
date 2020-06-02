774cef4a99c3150430e2268fa3cdab280eb128a3
invector4ik002 <Vedoone@mail.ru>
2020-06-03 05:00:29 +0700

    payload

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,10 @@ import { ITodo } from './interfaces';
 const App: React.FC = () => {
    
    const [todos, setTodos] = useState<ITodo[]>([]);// данный хук работает с интерфейсом ITodo
-
+/**
+ * 
+ * @param title 
+ */
    const addHandler = (title: string) => {
 
       const newTodo: ITodo = {// объект по типу интерфейса ITodo
@@ -24,6 +27,20 @@ const App: React.FC = () => {
       // стейт и пропсы работают в React ассинхронно,нет гарантии того что мы будем работать с придедущим стейтом!!!
       // setTodos([newTodo, ...todos]);// не очень корректно 
       setTodos((prev) => [newTodo, ...prev]); // такая запись гарантирует работу с предыдущим сотоянием , кыыылбэк замыкания :)
+   };
+/**
+ * 
+ * @param id 
+ */
+   const tooggleHandler = (id: number) => {
+
+   };
+/**
+ * 
+ * @param id 
+ */
+   const removeHanler = (id: number) => {
+
    };
 
    return (
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
+
+import { ITodo } from '../interfaces';
 /**
  * Описание того что есть пропсы :)
- * todos: any[], это будет массив
+ * todos: any[], это будет массив any-любой поправим
+ * todos: ITodo[], это будет массив по типу "модели (применяю термин из express)" ITodo
  */
 type TodoListProps = {
-   todos: any[]
+   // todos: any[]
+   todos: ITodo[]
 };
 /**
  * @param {todos} - массив-пропс :)
@@ -13,16 +17,22 @@ export const TodoList: React.FC<TodoListProps> = ({ todos }) => {
    return (
       <ul>
          {todos.map( (todo) => {
+
+            const classes = ['todo'];
+            if(todo.complited) {
+               classes.push('complited')
+            }
+
             return (
-               <li className="todo">
+               <li className={classes.join(' ')} key={todo.id}>
                   <label>   
-                     <input type='checkbox'/>
-                     <span></span>
+                     <input type='checkbox' checked={todo.complited}/>
+                     <span>{todo.title}</span>
                      <i className='material-icons red-text'>delete</i>
                   </label>
                </li>
             );
-         })};
+         })}
       </ul>
    );
 };
